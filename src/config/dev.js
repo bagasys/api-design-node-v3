898398a6a7fc7d21d58907fe166a4602ecf7b13c
Bagas Yanuar Sudrajad <bagasys3@gmail.com>
2020-09-24 18:14:48 +0700

    mongodb connection

--- src/config/dev.js
+++ src/config/dev.js
@@ -2,5 +2,6 @@ export const config = {
   secrets: {
     jwt: 'learneverything'
   },
-  dbUrl: 'mongodb://localhost:27017/api-design'
+  dbUrl:
+    'mongodb+srv://admin:kI7O9pMabmGk1I20@cluster0.wn2za.mongodb.net/api-nodejs-v3?retryWrites=true&w=majority'
 }
